1a3abbf5c40b62f878ca468b37a9ca4a04f2e842
robozavri <nika37912@hotmail.com>
2021-02-09 11:34:38 +0400

    [feat][add SingleChildScrollView]

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -8,39 +8,45 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: tranactions.map((tx) {
-        return Card(
-          child: Row(
-            children: <Widget>[
-              Container(
-                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                padding: EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                    border: Border.all(color: Colors.purple, width: 2)),
-                child: Text(
-                  '\$${tx.amount.toString()}',
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 13,
-                      color: Colors.purple),
-                ),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    tx.title,
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+    return Container(
+      height: 300,
+      child: SingleChildScrollView(
+        child: Column(
+          children: tranactions.map((tx) {
+            return Card(
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                    padding: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.purple, width: 2)),
+                    child: Text(
+                      '\$${tx.amount.toString()}',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 13,
+                          color: Colors.purple),
+                    ),
                   ),
-                  Text(DateFormat.yMMMd().format(tx.date),
-                      style: TextStyle(fontSize: 13, color: Colors.grey)),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        tx.title,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 16),
+                      ),
+                      Text(DateFormat.yMMMd().format(tx.date),
+                          style: TextStyle(fontSize: 13, color: Colors.grey)),
+                    ],
+                  )
                 ],
-              )
-            ],
-          ),
-        );
-      }).toList(),
+              ),
+            );
+          }).toList(),
+        ),
+      ),
     );
   }
 }
