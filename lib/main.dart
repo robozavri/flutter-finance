25f4f3b4fdd651a6e1df8f252e5780d50c125afc
robozavri <nika37912@hotmail.com>
2021-02-09 14:55:01 +0400

    [feat][finish-92]

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import './widgets/user_transactions.dart';
+import 'package:flutter_finance/widgets/new_transaction.dart';
+import 'package:flutter_finance/widgets/transaction_list.dart';
+import './widgets/new_transaction.dart';
+import './models/transaction.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,7 +16,77 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<Transaction> _userTransactions = [
+    Transaction(
+      id: 'otfghjkl;',
+      title: 'some transaction title 1',
+      amount: 25.25,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 'hnkyuuimu;',
+      title: 'some transaction title 2',
+      amount: 85.25,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 'vgrvgtg;',
+      title: 'some transaction title 3',
+      amount: 34.25,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 'vgrvgtg;',
+      title: 'some transaction title 4',
+      amount: 34.25,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 'vgrvgtg;',
+      title: 'some transaction title 5',
+      amount: 34.25,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 'vgrvgtg;',
+      title: 'some transaction title 6',
+      amount: 34.25,
+      date: DateTime.now(),
+    )
+  ];
+
+  void _addNewTransaction(String txTitle, double txAmount) {
+    final newTx = Transaction(
+      title: txTitle,
+      amount: txAmount,
+      date: DateTime.now(),
+      id: DateTime.now().toString(),
+    );
+
+    setState(() {
+      _userTransactions.add(newTx);
+    });
+  }
+
+  void _startAddNEwTransaction(BuildContext ctx) {
+    showModalBottomSheet(
+      context: ctx,
+      builder: (_) {
+        return GestureDetector(
+          onTap: () {},
+          child: NewTransaction(_addNewTransaction),
+          behavior: HitTestBehavior.opaque,
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,7 +95,7 @@ class MyHomePage extends StatelessWidget {
           actions: <Widget>[
             IconButton(
               icon: Icon(Icons.add),
-              onPressed: () {},
+              onPressed: () => _startAddNEwTransaction(context),
             )
           ],
         ),
@@ -37,12 +110,13 @@ class MyHomePage extends StatelessWidget {
                   elevation: 5,
                 ),
               ),
-              UserTransactions()
+              TransactionList(_userTransactions)
             ],
           ),
         ),
         floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-        floatingActionButton:
-            FloatingActionButton(child: Icon(Icons.add), onPressed: () {}));
+        floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.add),
+            onPressed: () => _startAddNEwTransaction(context)));
   }
 }
